9c376ebaeb938319c2a479d41a3156bfcd7f04c1
Flemming Krog Olsen <flemming@krogolsen.dk>
2021-12-04 08:08:55 +0100

    renamed routes to pages

--- src/index.tsx
+++ src/index.tsx
@@ -3,20 +3,14 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 
-import "./index.css";
+import "./theme/index.css";
+import { theme } from "./theme";
 
 import App from "./components/App";
-
-import Home from "./routes/home";
-import Blog from "./routes/blog";
-import CV from "./routes/cv";
-import Error404 from "./routes/error404";
-
-export const theme = {
-  main: "#123456",
-  activeColor: "#c8d1db",
-  inactiveColor: "#8f9ba7",
-};
+import Home from "./pages/home";
+import Blog from "./pages/blog";
+import CV from "./pages/cv";
+import Error404 from "./pages/error404";
 
 ReactDOM.render(
   <React.StrictMode>
